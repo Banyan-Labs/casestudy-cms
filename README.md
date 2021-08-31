1f1d55833470c8b81bba7bb8ef1d5aa2384c9ecb
Sarahunt89 <87195235+Sarahunt89@users.noreply.github.com>
2021-08-31 07:20:16 -0700

    Homepage Public

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Jessica Dickerson - Team Lead
+Stephanie Cook - Team Member
+Sarah Ramos - Team Member
---
+++ src/data/projectData.js
@@ -0,0 +1,17 @@
+
+export const projectData = [
+    {
+        name: "Indeed",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor veritatis accusamus explicabo eveniet impedit minima accusantium sapiente eius dignissimos harum!",
+        image: '../components/ProjectCard/Image/indeedHomepage.jpg'
+    },
+    {
+        name: "Manifold",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor veritatis accusamus explicabo eveniet impedit minima accusantium sapiente eius dignissimos harum!"
+    },
+    {
+        name: "ttv capital",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor veritatis accusamus explicabo eveniet impedit minima accusantium sapiente eius dignissimos harum!"
+    },
+
+  ]
