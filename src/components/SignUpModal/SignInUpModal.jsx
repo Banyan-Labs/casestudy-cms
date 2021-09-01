e365b920ec75b8457f098587f307a7ba87cb075f
StephanieKirshner <88007877+StephanieKirshner@users.noreply.github.com>
2021-09-01 10:27:03 -0700

    created sign in/up modals

---
+++ src/components/SignUpModal/SignInUpModal.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { ModalContainer, Content } from './style';
+import './style.js';
+
+const SignUpModal = ({ modalType }) => {
+
+    return (
+        <ModalContainer>
+            <Content>
+                {modalType === 1 ? <div>
+                    <h1>Sign Up For An Account</h1>
+                    <label>Email</label>
+                    <input type="text" id="email" name="email" required />
+                    <br />
+                    <label>Username</label>
+                    <input type="text" id="username" name="username" required />
+                    <br />
+                    <label>Password</label>
+                    <input type="password" id="password" name="password" required />
+                    <br />
+                    <br />
+                    <input id="submit" type="submit" value="Sign Up" />
+                </div> : <div>
+                    <h1>Sign In To Your Account</h1>
+                    <label>Username</label>
+                    <input type="text" id="username" name="username" required />
+                    <br />
+                    <label>Password</label>
+                    <input type="password" id="password" name="password" required />
+                    <br />
+                    <br />
+                    <input id="submit" type="submit" value="Sign In" />
+                </div>}
+            </Content>
+        </ModalContainer>
+
+    )
+}
+
+export default SignUpModal;
