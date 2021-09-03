6d584f7dfd5e4ab69b3efe78cce39ba94716b01a
jdickerson-banyan <75698869+jdickerson-banyan@users.noreply.github.com>
2021-09-03 09:59:13 -0700

    CC-fix: folder/file name changes, added initial route, minor changes

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+import './style.css';
+
+const HomePage = ({ data }) => {
+  const [modalType, setModalType] = useState(0);
+  return (
+    <div className='container'>
+      <button
+        className='signUpBtn'
+        onClick={() => setModalType(1)}
+        style={{ backgroundColor: '#9fb5d5' }}
+      >
+        Sign Up
+      </button>
+      <button
+        className='signInBtn'
+        onClick={() => setModalType(2)}
+        style={{ backgroundColor: '#9fb5d5' }}
+      >
+        Sign In
+      </button>
+
+      <div className='header'>
+        Project Case Study
+        {data.map((project) => (
+          <div className='cardBody' key={project.name}>
+            <div className='cardInfo'>
+              <div className='cardName'>{project.name}</div>
+              <div className='cardDescription'>{project.description}</div>
+            </div>
+
+            <div className='cardLogo'>
+              <img
+                src={project.image}
+                style={{ width: '400px', padding: '10%' }}
+                alt='Company Homepage'
+                className='cardImg'
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
