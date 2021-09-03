80c486cd2ef1a79cb9917625964ca7f7cae057c2
jdickerson-banyan <75698869+jdickerson-banyan@users.noreply.github.com>
2021-09-03 14:45:35 -0700

    CC-fix: updated app.js

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 
 import './style.css';
@@ -20,7 +22,6 @@ const HomePage = ({ data }) => {
       >
         Sign In
       </button>
-
       <div className='header'>
         Project Case Study
         {data.map((project) => (
