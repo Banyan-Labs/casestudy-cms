b5068bc7ad4d6e4e21e67b971e10a9a5508414a2
jdickerson-banyan <75698869+jdickerson-banyan@users.noreply.github.com>
2021-09-13 08:12:28 -0700

    CC-002: updated routes and link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import DetailPage from './components/DetailPage/DetailPage';
 import HomePage from './components/HomePage/HomePage';
+import InputForm from './components/InputForm/InputForm';
 import { projectData } from './data/projectData';
 
 function App() {
@@ -11,7 +12,10 @@ function App() {
         <Route path='/' exact>
           <HomePage data={projectData} buttonsToRender='frontend' />
         </Route>
-        <Route path='/:projectId' exact>
+        <Route path='/input-page' exact>
+          <InputForm />
+        </Route>
+        <Route path='/details/:projectId' exact>
           <DetailPage data={projectData} />
         </Route>
       </div>
